1ccefda0ee4290af1140c453cd224f28ea93c9ff
James Darling <james@abscond.org>
2024-10-08 13:30:56 +0100

    Add simple commend to send message straight to openai

--- Gemfile
+++ Gemfile
@@ -4,3 +4,6 @@ ruby '3.3.0'
 
 gem 'discordrb'
 gem 'dotenv'
+
+gem "langchainrb", "~> 0.17.1"
+gem 'ruby-openai'
--- bot.rb
+++ bot.rb
@@ -1,6 +1,13 @@
 require 'discordrb'
 require 'dotenv'
+require 'langchain'
+require 'openai'
 Dotenv.load
+
+llm = Langchain::LLM::OpenAI.new(
+  api_key: ENV["OPENAI_API_KEY"],
+  default_options: { temperature: 0.7, chat_completion_model_name: "gpt-4o" }
+)
 
 bot = Discordrb::Bot.new token: ENV['BOT_TOKEN'],
   client_id: ENV['CLIENT_ID'],
@@ -11,6 +18,16 @@ puts "This bot's invite URL is #{bot.invite_url}"
 bot.message(with_text: 'Ping!') do |event|
   p "received message: #{event.message.content}"
   event.respond 'Pong!'
+end
+
+bot.message(start_with: 'bot: ') do |event|
+  messages = [
+    { role: "system", content: "You are a helpful assistant." },
+    { role: "user", content: event.message.content.gsub("bot: ", "") }
+  ]
+  response = llm.chat(messages: messages)
+  chat_completion = response.chat_completion
+  event.respond chat_completion
 end
 
 bot.run
